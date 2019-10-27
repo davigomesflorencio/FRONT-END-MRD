1d619e0ecd28bcd7f2bc9deaf36405afca7ffcfc
Robson Teixeira <robson.teixeira2000@gmail.com>
2019-10-27 15:49:48 -0300

    Criação do meu de navegação

--- mrd-advogados/src/SubstituirAdvogado.js
+++ mrd-advogados/src/SubstituirAdvogado.js
@@ -6,12 +6,14 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import CreateIcon from '@material-ui/icons/Create';
 import Typography from '@material-ui/core/Typography';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 class SubstituirAdvogado extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
+        this.state =  {
             //   inicializa a lista de processo com uma lista estática
             // Essa parte deve ser alterada para pegar no banco
             processes: [{ processo: 1, cliente: "a" }, { processo: 2, cliente: "b" }, { processo: 3, cliente: "c" }],
@@ -34,6 +36,8 @@ class SubstituirAdvogado extends React.Component {
     render() {
         return (
             <div>
+                {/* Menu de navegação */}
+                <Navbar/>
                 {/* Renderiza o titulo da página */}
                 <Typography variant="h4" style={style}>Processos</Typography>
                 {/* constrói uma tabela com a identifiação do processo, o cliente associado ao processo
---
+++ mrd-advogados/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">MRD - Advogados</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/"> Inicio </a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/substituiradvogado"><span className="sr-only">(current)</span>substituir advogado</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/"> Custas </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/"> Processos </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
