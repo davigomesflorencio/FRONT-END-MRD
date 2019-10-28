a775eb122705b71433afeeade54b520ee165e90b
Robson Teixeira <robson.teixeira2000@gmail.com>
2019-10-27 21:20:15 -0300

    Criação do input de busca

--- mrd-advogados/src/SubstituirAdvogado.js
+++ mrd-advogados/src/SubstituirAdvogado.js
@@ -9,20 +9,32 @@ import Typography from '@material-ui/core/Typography';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 
+function searching(term){
+    return function(x){
+        return x.cliente.toLowerCase().includes(term.toLowerCase()) || !term;
+    }
+}
+
 class SubstituirAdvogado extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state =  {
+        this.state = {
             //   inicializa a lista de processo com uma lista estática
             // Essa parte deve ser alterada para pegar no banco
             processes: [{ processo: 1, cliente: "a" }, { processo: 2, cliente: "b" }, { processo: 3, cliente: "c" }],
+            term: '',
         }
 
         //   Define o contexto de setProcesses
         this.setProcesses = this.setProcesses.bind(this);
+        this.searchHandler = this.searchHandler.bind(this);
     }
 
+    // Metodo que captura o termo que será feita a busca
+    searchHandler(event){
+        this.setState({term: event.target.value});
+    }
     // Metodo referente ao alterar o advogado
     editAdvogado(processo) {
 
@@ -34,12 +46,17 @@ class SubstituirAdvogado extends React.Component {
     }
 
     render() {
+        const {term, processes} = this.state;
         return (
             <div>
                 {/* Menu de navegação */}
-                <Navbar/>
+                <Navbar />
                 {/* Renderiza o titulo da página */}
-                <Typography variant="h4" style={style}>Processos</Typography>
+                <Typography variant="h4" style={style}>LISTA DE PROCESSOS</Typography><br />
+                <form>
+                    <input type="text" class="form-control" id="examplesearchFilter" value={term}
+                    placeholder="Pesquisar pelo nome do cliente" onChange={this.searchHandler} />
+                </form>
                 {/* constrói uma tabela com a identifiação do processo, o cliente associado ao processo
                   e um opção para alterar o advogado do processo*/}
                 <Table>
@@ -50,7 +67,7 @@ class SubstituirAdvogado extends React.Component {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.processes.map(row => (
+                        {processes.filter(searching(term)).map(row => (
                             <TableRow key={row.processo}>
                                 <TableCell component="th" scope="row">
                                     {row.processo}
